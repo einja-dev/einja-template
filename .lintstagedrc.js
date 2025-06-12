2e96a17a602bf098405fd6eb5cc549d04adf64ba
masaya48 <msya0408@gmail.com>
2025-06-12 09:37:17 +0900

    fix lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,5 @@ module.exports = {
 	"**/*.{ts,tsx}": () => "npm run typecheck",
 
 	// Biomeによるリントとフォーマット
-	"**/*.{js,jsx,ts,tsx,json,md}": [
-		"npx @biomejs/biome check --apply",
-		"npx @biomejs/biome format --write",
-	],
+	"**/*.{js,jsx,ts,tsx,json,md}": ["npm run lint:fix", "npm run format:fix"],
 };
